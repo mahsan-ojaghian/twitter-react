189fad2e72b087ca035afe79edc26014ed37e2f7
Mahsan Ojaghian <77487326+mahsan-ojaghian@users.noreply.github.com>
2021-07-31 09:20:04 +0430

    layouts componenet added

---
+++ components/layouts/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Layout() {
+  return (
+    <div>
+      <div>left sidebar</div>
+      <div>main part</div>
+      <div>right sidebar</div>
+    </div>
+  );
+}
+export default Layout;
